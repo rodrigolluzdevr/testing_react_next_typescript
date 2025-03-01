1cb79ff6416350bbc4b57b79c12123b54c1d5d3e
Rodrigo Luz <rodrigolluzdevr@gmail.com>
2025-03-01 11:40:15 -0300

    first commit.

---
+++ src/components/Card.tsx
@@ -0,0 +1,21 @@
+interface CardProps {
+    product: string;
+    value: number;
+    descount: number;
+    funct: any;
+}
+
+export default function Card(props: CardProps) {
+    return (
+        <div className='flex flex-col border border-black-700 rounded-sm p-1'>
+            <div>Product: {props.product}</div>
+            <div>Value: ${props.value}</div>
+            {props.descount>0?(
+                <div>
+                    <div>Desc.: ${props.descount}</div>
+                    <div>Price With Desc.: ${props.funct(props.value, props.descount)}</div>
+                </div>
+            ):""}
+        </div>
+    )
+}
