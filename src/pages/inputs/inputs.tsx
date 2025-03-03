cd8f82fc926ef6ae4075c56bd98f11775dbf6eed
Rodrigo Luz <rodrigolluzdevr@gmail.com>
2025-03-03 13:43:53 -0300

    feat: new feature - form.

---
+++ src/pages/inputs/inputs.tsx
@@ -0,0 +1,40 @@
+import { useState } from "react"
+
+const courses = ["HTML", "React", "Vue", "C#", "Typescript", "Arduino"]
+
+const xcourses = [
+    {"course": "HTML"},
+    {"course": "React"},
+    {"course": "Vue"},
+    {"course": "C#"},
+    {"course": "Typescript"},
+    {"course": "Arduino"},
+]
+
+export default function inputs() {
+    const [ name, setName ] = useState<string>("")
+    const [ course, setCourse ] = useState<string>("")
+
+    function fcourses() {
+        return xcourses.map((c: any) => {
+            return <option value={c.course}>{c.course}</option>
+        })
+    }
+
+    return (
+        <div >
+            <div className="form">
+                <label className="formlabel">Name</label>
+                <input className="forminput" type="text" value={name} onChange={(evt) => setName(evt.target.value)}></input>
+            </div>
+            <div className="form">
+                <label className="formlabel">Course</label>
+                <select className="formselect" value={course} onChange={(evt) => setCourse(evt.target.value)}>
+                    {
+                        fcourses()
+                    }
+                </select>
+            </div>
+        </div>
+    )
+}
